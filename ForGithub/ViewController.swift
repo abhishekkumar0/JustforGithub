3b64a2a80bd642f107d566ea94400b089f92ecb8
Abhi <abhi7042419282@gmail.com>
2021-05-13 12:49:54 +0530

    this is after some changes in the viewController and I am loving it man

--- ForGithub/ViewController.swift
+++ ForGithub/ViewController.swift
@@ -13,7 +13,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        <#code#>
+    }
+    override func viewLayoutMarginsDidChange() {
+        <#code#>
+    }
 
 }
 
